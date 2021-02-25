8449e94d711720ddf3d5dda36d9487d0c8e24a35
zid3 <zironside@d3corp.com>
2021-02-25 08:37:55 -0500

    use nebula thread class

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 from windows import is_admin
-from nebula import runNebula
+from nebula import Nebula
 import threading
 import os
 from storage import Storage
 import pystray
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from pystray import Menu, MenuItem
 from plyer import filechooser
 import pathlib
@@ -21,29 +21,19 @@ thread = None
 threadDone = True
 logStr = ''
 
-def create_image(color=250):
+def create_image(type="off"):
     # Generate an image and draw a pattern
-    width=16
-    height=16
-    color1=20
-    color2=color
-    image = Image.new('RGB', (width, height), color1)
-    dc = ImageDraw.Draw(image)
-    dc.rectangle(
-        (width // 2, 0, width, height // 2),
-        fill=color2)
-    dc.rectangle(
-        (0, height // 2, width // 2, height),
-        fill=color2)
+    image = Image.open(directory+os.sep+"resources"+os.sep+"icon-"+type+".png")
 
     return image
 
 def setup(icon):
     icon.visible = True
 def log(line):
-    file1 = open(str(directory)+os.sep+'thread.log', 'a+')
+    file1 = open(str(directory)+os.sep+'nebula_gui.log', 'a+')
     file1.write(line) 
     file1.close() 
+    
 def tDone():
     global threadDone
     threadDone=True
@@ -51,16 +41,19 @@ def tDone():
 def connect(icon, item):
     global thread, threadDone
     if threadDone:
-        thread = threading.Thread(target=runNebula, args=("nebula", log, store.get('executable_path'), store.get('config_path'), directory, tDone))
+        thread = Nebula(log, tDone, directory, store.get('config_path'))
         thread.daemon = True
         thread.start()
-        icon.icon = create_image((51,255,51))
+        icon.icon = create_image("connected")
         threadDone=False
 def config(icon, item):
     paths = filechooser.open_file(title="Choose Nebula Config")    
     if len(paths) > 0:
         store.add('config_path', paths[0])
 def quit(icon, item):
+    global thread
+    if thread is not None:
+        thread.disconnect()
     icon.stop()
 def menu():
     return Menu(
--- nebula.py
+++ nebula.py
@@ -1,6 +1,7 @@
 import subprocess, time
 import pathlib
 import os
+import threading
 
 def popen(cmd):
     startupinfo = subprocess.STARTUPINFO()
@@ -8,14 +9,35 @@ def popen(cmd):
     process = subprocess.Popen(cmd, startupinfo=startupinfo, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     return process
 
-def runNebula(threadName, lineCallback, executable, config, directory, done):
+class Nebula(threading.Thread):
+    stop=False
+    def __init__(self, lineCallback, doneCallback, directory, config):
+        threading.Thread.__init__(self)
+        self.threadID = "nebula"
+        self.setName("nebula")
+        self.lineCallback = lineCallback
+        self.doneCallback = doneCallback
+        self.directory = directory
+        self.config = config
+    def run(self):
+        self.lineCallback("Nebula running\n")
+        self.startProcess()
+    
+    def startProcess(self):
+        print(str(self.directory)+os.sep+"nebula.exe")
+        proc = popen([str(self.directory)+os.sep+"nebula.exe", "--config", self.config])
+        for line in iter(proc.stdout.readline, ''):
+            if line.decode('utf-8') != '':
+                self.lineCallback(line.decode('utf-8'))
+            if proc.poll() is not None:
+                self.doneCallback()
+                proc.kill()
+                break
+            if self.stop:
+                self.doneCallback()
+                self.lineCallback("Stopping")
+                proc.kill()
+                break
 
-    lineCallback("Nebula running\n")
-    proc = popen([str(directory)+os.sep+"nebula.exe", "--config", config])
-    for line in iter(proc.stdout.readline, ''):
-        if line.decode('utf-8') != '':
-                lineCallback(line.decode('utf-8'))
-        if proc.poll() is not None:
-            done()
-            proc.kill()
-            break
+    def disconnect(self):
+        self.stop=True
